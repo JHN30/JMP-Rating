f4d3dfb1ea000dbd38f56707b662c7430f0f5d76
Jovan Mitic <johanmit03@gmail.com>
2025-11-05 15:48:44 +0100

    Create euroleague_players table in silver schema
    This script creates the 'euroleague_players' table in the 'silver' schema, dropping it if it already exists. It defines various columns related to player statistics.

---
+++ scripts/silver/ddl_silver_euroleague.sql
@@ -0,0 +1,76 @@
+/*
+===============================================================================
+DDL Script: Create Silver Tables
+===============================================================================
+Script Purpose:
+    This script creates tables in the 'silver' schema, dropping existing tables 
+    if they already exist.
+	  Run this script to re-define the DDL structure of 'bronze' Tables
+===============================================================================
+*/
+
+IF OBJECT_ID('silver.euroleague_players', 'U') IS NOT NULL
+    DROP TABLE silver.euroleague_players;
+GO
+
+CREATE TABLE silver.euroleague_players (
+    season_player_id                   NVARCHAR(50),
+    season_code                        NVARCHAR(30),
+    player_id                          NVARCHAR(50),
+    player_last_name                   NVARCHAR(50),
+	player_first_name                  NVARCHAR(50),
+    team_id                            NVARCHAR(20),
+
+    games_played                       FLOAT,
+    games_started                      FLOAT,
+    minutes                            FLOAT,
+
+    points                             INT,
+    two_points_made                    INT,
+    two_points_attempted               INT,
+    three_points_made                  INT,
+    three_points_attempted             INT,
+    free_throws_made                   INT,
+    free_throws_attempted              INT,
+
+    offensive_rebounds                 INT,
+    defensive_rebounds                 INT,
+    total_rebounds                     INT,
+
+    assists                            INT,
+    steals                             INT,
+    turnovers                          INT,
+    blocks_favour                      INT,
+    blocks_against                     INT,
+    fouls_committed                    INT,
+    fouls_received                     INT,
+    valuation                          INT,
+    plus_minus                         INT,
+
+    minutes_per_game                   FLOAT,
+    points_per_game                    FLOAT,
+    two_points_made_per_game           FLOAT,
+    two_points_attempted_per_game      FLOAT,
+    two_points_percentage              FLOAT,
+    three_points_made_per_game         FLOAT,
+    three_points_attempted_per_game    FLOAT,
+    three_points_percentage            FLOAT,
+    free_throws_made_per_game          FLOAT,
+    free_throws_attempted_per_game     FLOAT,
+    free_throws_percentage             FLOAT,
+
+    offensive_rebounds_per_game        FLOAT,
+    defensive_rebounds_per_game        FLOAT,
+    total_rebounds_per_game            FLOAT,
+    assists_per_game                   FLOAT,
+    steals_per_game                    FLOAT,
+    turnovers_per_game                 FLOAT,
+    blocks_favour_per_game             FLOAT,
+    blocks_against_per_game            FLOAT,
+    fouls_committed_per_game           FLOAT,
+    fouls_received_per_game            FLOAT,
+    valuation_per_game                 FLOAT,
+    plus_minus_per_game                FLOAT,
+	dwh_create_date					   DATETIME2 DEFAULT GETDATE()
+);
+GO
